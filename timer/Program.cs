19eb7f4eb42600028ea5a78d76963889644f0a85
James Thach <j.thach000@gmail.com>
2023-01-06 00:23:18 -0500

    Initial project setup and menu creation

---
+++ timer/Program.cs
@@ -0,0 +1,30 @@
+﻿/* TODO:
+ * prompt user to choose stopwatch or timer function
+ * [] print a menu
+ * [] allow user to input a menu choice
+ * 
+ * choosing the stopwatch function begins updating a printed line with time
+ * choosing the timer functions shows a line of printed time decreasing
+ * pressing enter key stops current function
+ */
+
+void MenuPrint()
+{
+    Console.WriteLine("1 - Stopwatch");
+    Console.WriteLine("2 - Timer");
+    Console.WriteLine("0 - Exit");
+}
+
+int MenuReadNumber()
+{
+    Console.Write("Please input a choice: ");
+    int menuNumber = Convert.ToInt32(Console.ReadLine());
+    return menuNumber;
+}
+
+int menuChoice;
+do
+{
+    MenuPrint();
+    menuChoice = MenuReadNumber();
+} while (menuChoice != 0);
